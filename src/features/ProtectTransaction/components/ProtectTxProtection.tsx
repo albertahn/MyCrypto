282a8f424eb53b7e54cf5e4962a3b9fe80343f2c
Frederik Bolding <frederik.bolding@gmail.com>
2020-05-19 23:17:16 +0200

    Small copy change

--- src/features/ProtectTransaction/components/ProtectTxProtection.tsx
+++ src/features/ProtectTransaction/components/ProtectTxProtection.tsx
@@ -19,6 +19,8 @@ import { ProtectTxError } from '..';
 import { ProtectTxMissingInfo } from './ProtectTxMissingInfo';
 import PoweredByNansen from './PoweredByNansen';
 
+import bulletIcon from 'assets/images/icn-bullet.svg';
+
 const SProtectionThisTransaction = styled(ProtectTxBase)`
   .description-text {
     max-width: 300px;
@@ -71,7 +73,7 @@ const BulletList = styled.ul`
     padding-left: ${SPACING.SM};
     margin-left: -${SPACING.SM};
     margin-bottom: 15px;
-    background-image: url('~assets/images/icn-bullet.svg');
+    background-image: url(${bulletIcon});
     background-position: 0 ${SPACING.SM};
     background-size: 5px 5px;
     background-repeat: no-repeat;
